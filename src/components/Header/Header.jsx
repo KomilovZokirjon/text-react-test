80b9d2808259a307bb8dcc4dd6bab054f1bb53a6
KomilovZokirjon <zokirjonkomilov32@gmail.com>
2023-02-08 17:36:41 +0300

    text react

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+        <>
+          <div>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sapiente, quia adipisci consectetur id earum! Blanditiis officia, error sint culpa ut animi autem, vel debitis illo pariatur quasi hic tenetur.</p>  
+          </div>        
+        </>
+    )
+}
